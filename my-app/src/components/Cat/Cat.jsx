aa3911d19cebb0e946fcb6a3d30056bce42444ee
stepanamz <stepanamz@mail.ru>
2022-05-29 23:30:26 +0700

    Исправлена Верстка

--- my-app/src/components/Cat/Cat.jsx
+++ my-app/src/components/Cat/Cat.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import like from "../../like.svg";
+import likeHovered from "../../likehovered.svg";
 
 const Cat = ({ onCatClick, ...data }) => {
+  const [isActive, setIsActive] = useState(false);
   const handleLikeCat = (clickedElement) => (event) => {
     const favorits = JSON.parse(localStorage.getItem("favorits")) || [];
     if (!favorits.find((item) => item.id === clickedElement.id)) {
@@ -15,14 +17,24 @@ const Cat = ({ onCatClick, ...data }) => {
       localStorage.setItem("favorits", JSON.stringify(newData));
     }
   };
-
+  const handleHoverIcon = () => {
+    setIsActive(true);
+  };
+  const handleMouseLeave = () => {
+    setIsActive(false);
+  };
+  const isFavorite = JSON.parse(localStorage.getItem("favorits") || []).find(
+    (item) => item.id === data.id
+  );
   return (
     <div className="cat">
       <div className="cat__wrapper">
         <img
           className="cat__like"
           onClick={handleLikeCat(data)}
-          src={like}
+          onMouseOver={handleHoverIcon}
+          onMouseLeave={handleMouseLeave}
+          src={isActive || isFavorite ? likeHovered : like}
           alt=""
         />
         <img src={data.url} alt="" />
